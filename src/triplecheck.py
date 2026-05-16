f406247443fc9fe0e88b317725f1444a515951e3
Lucuma13 <alterluigi@gmail.com>
2026-05-16 22:41:54 +0100

    create github action

--- src/triplecheck.py
+++ src/triplecheck.py
@@ -38,7 +38,7 @@ from pathlib import Path
 import blake3 as _blake3
 import xxhash as _xxhash
 
-__version__ = "2.0.0a6"
+__version__ = "2.0.0a7"
 
 # ---------------------------------------------------------------------------
 # Type aliases
